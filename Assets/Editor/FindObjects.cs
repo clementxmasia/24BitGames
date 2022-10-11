b4dec8a6e85928edcb71dc59e47fb34fb1a1d125
Xhanyana Clement Masia <clementxmasia@gmail.com>
2022-10-11 09:18:44 +0200

    Add files via upload

---
+++ Assets/Editor/FindObjects.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+public class FindObjects : EditorWindow
+{
+
+    [MenuItem("Window/FindObjectsMeshes")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(FindObjects));
+    }
+
+    public void OnGUI()
+    {
+
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Find object with Meshes"))
+        {
+
+            foreach (GameObject obj in Object.FindObjectsOfType(typeof(GameObject)))
+            {
+               
+                Debug.Log(obj.name);
+                if (obj.GetComponent<MeshFilter>().mesh != null)
+                {
+                    Debug.Log(" : Yes, I have a mesh! ");
+                    rootVisualElement.Add(new Label(obj.name));
+                }
+                else
+                {
+                    Debug.Log(" : Nope /wrist");
+                }
+                //Debug.Log("Test if it gets here to point 1");
+
+            }
+        }
+    }
+
+    /*private static void FindObjects()
+    {
+
+        foreach (GameObject obj in Object.FindObjectsOfType(typeof(GameObject)))
+        {
+            Debug.Log(obj.name);
+            if (obj.GetComponent<MeshFilter>().mesh != null)
+            {
+                Debug.Log(" : Yes, I have a mesh! ");
+            }
+            else
+            { 
+                Debug.Log(" : Nope /wrist");
+            }
+            //Debug.Log("Test if it gets here to point 1");
+        }
+
+
+    }*/
+}
